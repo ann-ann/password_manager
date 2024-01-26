6e3d8fc4c1db3494be9b46ba868aaf8d713a712b
ann-ann <saddy666@gmail.com>
2024-01-26 23:11:30 +0200

    passwords crud

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,7 +1,11 @@
 class Password < ApplicationRecord
-  has_many :user_passwords
+  has_many :user_passwords, dependent: :destroy
   has_many :users, through: :user_passwords
 
   encrypts :username, deterministic: true
   encrypts :password
+
+  validates :username, presence: true
+  validates :url, presence: true
+  validates :password, presence: true
 end
